e56a8b81ee782ce76170c871668588cd33f0b731
tambemrunal <mrunaltambe845@gmail.com>
2025-04-01 22:36:10 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./navbar.css";
+import "./Navbar.css";
 
+//helo
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1 className="logo">EduVerse</h1>
       <ul className="nav-links">
-        <li><NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>Home</NavLink></li>
-        <li><NavLink to="/course" className={({ isActive }) => isActive ? "active-link" : ""}>Courses</NavLink></li>
-        <li><NavLink to="/blog" className={({ isActive }) => isActive ? "active-link" : ""}>Blog</NavLink></li>
-        <li><NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : ""}>About Us</NavLink></li>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/course"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Courses
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/blog"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            About Us
+          </NavLink>
+        </li>
       </ul>
       <div className="auth-buttons">
         <button className="login">Log-in</button>
